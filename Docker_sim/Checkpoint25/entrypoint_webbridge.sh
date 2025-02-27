79271ad9d771bdd02457f5b6d216c6ca95396909
peerajak <peerajak@gmail.com>
2025-02-27 08:30:12 +0700

    SUCCESS on bringing Robot model, with correct TF from ROS2 bridge_r1 bridge_web to web

--- Docker_sim/Checkpoint25/entrypoint_webbridge.sh
+++ Docker_sim/Checkpoint25/entrypoint_webbridge.sh
@@ -2,14 +2,12 @@
 
 source /ros1_ws/devel/setup.bash
 
-rosparam load /ros1_ws/bridge.yaml &
-
+rosparam load /ros1_ws/bridge.yaml 
+echo "$(date +'[%Y-%m-%d %T]') robot_description param..."
+roslaunch ur_description --wait robot_description.launch 
 echo "$(date +'[%Y-%m-%d %T]') Starting rosbridge server..."
 roslaunch course_web_dev_ros --wait web2.launch &
 echo "$(date +'[%Y-%m-%d %T]') Starting tf2_web server..." 
 roslaunch course_web_dev_ros --wait tf2_web.launch  &
-echo "$(date +'[%Y-%m-%d %T]') Starting action server..." 
-#rosrun course_web_dev_ros tortoisebot_action_server.py &
-roslaunch ur_description --wait robot_description.launch &
 
 rosrun rqt_image_view rqt_image_view /wrist_rgbd_depth_sensor/image_raw 
