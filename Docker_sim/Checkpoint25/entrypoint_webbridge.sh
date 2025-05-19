34f2e451f4c7c3599578a71bec5e8291021f27d4
peerajak <peerajak@gmail.com>
2025-05-19 15:38:08 +0700

    renew docker sim

--- Docker_sim/Checkpoint25/entrypoint_webbridge.sh
+++ Docker_sim/Checkpoint25/entrypoint_webbridge.sh
@@ -1,13 +1,13 @@
 #! /bin/bash 
 
 source /ros1_ws/devel/setup.bash
+roslaunch launch_cp25_ros1 ros1_tf2_webbridge.launch.xml
+# rosparam load /ros1_ws/bridge.yaml 
+# echo "$(date +'[%Y-%m-%d %T]') robot_description param..."
+# roslaunch ur_description --wait robot_description.launch 
+# echo "$(date +'[%Y-%m-%d %T]') Starting rosbridge server..."
+# roslaunch course_web_dev_ros --wait web2.launch &
+# echo "$(date +'[%Y-%m-%d %T]') Starting tf2_web server..." 
+# roslaunch course_web_dev_ros --wait tf2_web.launch  &
 
-rosparam load /ros1_ws/bridge.yaml 
-echo "$(date +'[%Y-%m-%d %T]') robot_description param..."
-roslaunch ur_description --wait robot_description.launch 
-echo "$(date +'[%Y-%m-%d %T]') Starting rosbridge server..."
-roslaunch course_web_dev_ros --wait web2.launch &
-echo "$(date +'[%Y-%m-%d %T]') Starting tf2_web server..." 
-roslaunch course_web_dev_ros --wait tf2_web.launch  &
-
-rosrun rqt_image_view rqt_image_view /wrist_rgbd_depth_sensor/image_aruco_frame
+# rosrun rqt_image_view rqt_image_view /wrist_rgbd_depth_sensor/image_aruco_frame
