fab71103918b7f5ebf185d323cd3f484ed9b3807
peerajak <peerajak@gmail.com>
2025-11-01 07:58:45 +0700

    hole detection

--- my_tf_aruco/scripts/hole_to_camlink_baselink_tf_pub_service.py
+++ my_tf_aruco/scripts/hole_to_camlink_baselink_tf_pub_service.py
@@ -230,8 +230,8 @@ class HoleToCamlinkTF(Node):
         rows = gray.shape[0]
         # print('detecting holes...')
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
-                                param1=40, param2=80,
-                                minRadius=15, maxRadius=30)
+                                param1=45, param2=25,
+                                minRadius=10, maxRadius=30)
         corners = []
         centers = []
         bounding_box_ids = []
